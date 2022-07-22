d13d44d26d4cb6f84b4ccbcd3bd8aaa25b266760
Jan Gruenwaldt <jgruen02@tufts.edu>
2022-07-22 13:49:11 +0200

    more web fixes

--- packages/convenient_test_dev/lib/src/utils/snapshot.dart
+++ packages/convenient_test_dev/lib/src/utils/snapshot.dart
@@ -3,11 +3,16 @@ import 'dart:ui' as ui;
 import 'package:collection/collection.dart';
 import 'package:convenient_test/convenient_test.dart';
 import 'package:convenient_test_common/convenient_test_common.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 Future<List<int>> takeSnapshot({Future<void> Function()? pumper}) async {
+  if (kIsWeb) {
+    // snapshots can't be viewed on web anyways
+    return [];
+  }
   final element = _findElement();
   final image = await _captureImageFromElement(element, pumper: pumper);
   final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
@@ -18,12 +23,15 @@ Future<List<int>> takeSnapshot({Future<void> Function()? pumper}) async {
 Element _findElement() {
   final finder = find.byType(ConvenientTestImageCaptureWrapper);
   final element = finder.evaluate().singleOrNull;
-  if (element == null) throw Exception('Please put `ConvenientTestWrapperWidget` in the widget tree near root');
+  if (element == null)
+    throw Exception(
+        'Please put `ConvenientTestWrapperWidget` in the widget tree near root');
   return element;
 }
 
 /// NOTE ref [flutter_test :: _matchers_io.dart :: captureImage]
-Future<ui.Image> _captureImageFromElement(Element element, {Future<void> Function()? pumper}) async {
+Future<ui.Image> _captureImageFromElement(Element element,
+    {Future<void> Function()? pumper}) async {
   assert(element.renderObject != null);
   var renderObject = element.renderObject!;
   while (!renderObject.isRepaintBoundary) {
@@ -34,7 +42,8 @@ Future<ui.Image> _captureImageFromElement(Element element, {Future<void> Functio
   // see #234
   // see https://github.com/fzyzcjy/yplusplus/issues/4286#issuecomment-1170797044
   while (renderObject.debugNeedsPaint && pumper != null) {
-    Log.i('captureImageFromElement', 'see debugNeedsPaint==true and has pumper, thus pump');
+    Log.i('captureImageFromElement',
+        'see debugNeedsPaint==true and has pumper, thus pump');
     await pumper();
   }
 
